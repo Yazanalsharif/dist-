02ac9074c80c21aa929b252576fab5652a7f3cff
yazan alsharif <yazanalshariiif@gmail.com>
2022-09-25 18:05:45 +0300

    dist folder to be added on the root falconswaps project

---
+++ constants.d.ts
@@ -0,0 +1,35 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    TESTNET = 97
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x070643bBa42e0fb7fe4f1872d61C56D83e11Aea8";
+export declare const INIT_CODE_HASH = "0xc5eebda1df1f5306524de22c976b602fa0c2abca4e1e864c3108e112f41abd8b";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
